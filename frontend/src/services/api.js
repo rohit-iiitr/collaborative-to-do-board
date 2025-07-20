bb3e9273636241214a2eeb96e9fc1f39eee39662
Rohit_Patel_IIITR <127647959+rohit-iiitr@users.noreply.github.com>
2025-07-20 18:29:21 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://localhost:5000/api"
+const API_BASE_URL = "https://collaborative-to-do-board-jukp.onrender.com/api"
 
 // Helper function to get auth headers
 const getAuthHeaders = () => {
