90a518a9b6aba03dd6ffcace4c7367c187f3c28d
Rohit_Patel_IIITR <127647959+rohit-iiitr@users.noreply.github.com>
2025-07-20 18:43:41 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ const app = express()
 const server = createServer(app)
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://collaborative-to-do-board-1.onrender.com",
     methods: ["GET", "POST", "PUT", "DELETE"],
   },
 })
